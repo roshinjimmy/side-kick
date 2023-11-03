67860629335a8c3401025f3f133db933eb251a25
Roshin Jimmy <roshinjimmy14@gmail.com>
2023-11-03 14:48:25 +0530

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer class="site-footer">
+      <div class="social-links">
+        <a href="link" target="_blank">
+          Twitter
+        </a>
+        <a href="link" target="_blank">
+          Facebook
+        </a>
+        <a href="link" target="_blank">
+          Instagram
+        </a>
+      </div>
+
+      <div class="contact-info">
+        <p>Contact Us:</p>
+        <p>Email: email id</p>
+        <p>Phone: 1234567890</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div class="hero-section">
+      <h1>Empowering Communication, Fostering Inclusivity</h1>
+      <img src="../assets/images/hero-image.jpg" alt="Diverse individuals interacting happily"/>
+    </div>
+  );
+};
+
+export default HeroSection;

7f7b0d236a7a2928f32095209cf4edc11c497e19
Roshin Jimmy <roshinjimmy14@gmail.com>
2023-11-04 20:23:54 +0530

    Scroll event useEffect

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./HeroSection.css";
+import heroImage from '../assets/images/hero-image.jpg';
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
       <h1>Empowering Communication, Fostering Inclusivity</h1>
-      <img src="../assets/images/hero-image.jpg" alt="Diverse individuals interacting happily"/>
+      <img src={heroImage} alt="Diverse individuals interacting happily"/>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
 
 const Home = () => {
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    const windowHeight = window.innerHeight;
+    const fullHeight = document.body.offsetHeight;
+    const scrollPosition = window.scrollY;
+
+    if (windowHeight + scrollPosition >= fullHeight) {
+      setShowFooter(true);
+    } else {
+      setShowFooter(false);
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <Footer />
+      {showFooter && <Footer />}
     </div>
   );
 };

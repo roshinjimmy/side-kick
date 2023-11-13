8ce76cdd56af6e6e3d3eb569bb1d6032cb17e3d7
Roshin Jimmy <roshinjimmy14@gmail.com>
2023-11-13 19:19:41 +0530

    Update heroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./HeroSection.css";
-import heroImage from '../assets/images/hero-image.jpg';
+import vector from "../assets/images/vector.svg";
+import heroImage from "../assets/images/hero-image.svg";
 
 const HeroSection = () => {
   return (
-    <div className="hero-section">
-      <h1>Empowering Communication, Fostering Inclusivity</h1>
-      <img src={heroImage} alt="Diverse individuals interacting happily"/>
+    <div className="landing">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <img className="vector" alt="Vector" src={vector} />
+          <div className="empowering">
+            Empowering Communication. <br />
+            Fostering Inclusivity.
+          </div>
+          <img
+            className="community-cuate"
+            alt="Community cuate"
+            src={heroImage}
+          />
+          <div className="rectangle" />
+          <div className="text-wrapper-3">Learn More.</div>
+        </div>
+      </div>
     </div>
   );
 };
